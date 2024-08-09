f03d3028390b2cdb3b3dac791a6968679f816a56
Sadid <66120003+sadid369@users.noreply.github.com>
2024-08-10 01:07:34 +0600

    complete

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
-
+const loader = document.getElementById("loader");
 let apiQuotes = [];
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+function removeLoadingSpinner() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 function newQuote() {
+  showLoadingSpinner();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   if (!quote.author) {
     authorText.innerText = "Unknown";
@@ -19,9 +28,11 @@ function newQuote() {
     quoteText.classList.remove("long-quote");
   }
   quoteText.innerText = quote.text;
+  removeLoadingSpinner();
 }
 
 async function getQuotes() {
+  showLoadingSpinner();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -40,4 +51,5 @@ newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
 // on Load
 getQuotes();
+
 // finished @ 22. new creating
